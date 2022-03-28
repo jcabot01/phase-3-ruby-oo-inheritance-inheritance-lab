1984b19092d506ab0b5b700271f26f2d2c1b89e3
JonCabot <jfc0053@gmail.com>
2022-03-27 20:36:06 -0500

    completed assignment

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,11 @@
-class Student 
+class Student < User
+  attr_reader :knowledge
 
+  def initialize
+    @knowledge = []
+  end
+
+  def learn(learned_knowledge)
+    knowledge << learned_knowledge
+  end
 end
